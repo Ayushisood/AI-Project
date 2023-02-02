77e96f6a80d532a7e35be2200a5e5ba768be198a
Ayushi <ayushi@brenolabs.com>
2023-02-02 22:11:51 +0530

    fix

--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -1,12 +1,4 @@
 import styled from "styled-components";
-import { keyframes } from "styled-components";
-
-const breatheAnimation = keyframes`
- 0% { height: 100px; width: 100px; }
- 30% { height: 400px; width: 400px; opacity: 1 }
- 40% { height: 405px; width: 405px; opacity: 0.3; }
- 100% { height: 100px; width: 100px; opacity: 0.6; }
-`;
 
 export const HomeWrapper = styled.div`
   background-color: #87ceeb;
